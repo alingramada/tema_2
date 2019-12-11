00bf1272b0589137db34b02a1cafa0d2df1b1f81
HOME's <HOME's@DESKTOP-HF90GH9>
2019-12-12 00:10:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//metoda concat
+            string nonculoare1 = "alb";
+            string nonculoare2 = "negru";
+            string nonculoare = string.Concat(nonculoare1, nonculoare2);
+            Console.WriteLine(nonculoare);
+
+            //metoda "trim"
+            string cut = "  I have a string";
+            Console.WriteLine(cut.Trim());
+
+            //metoda replace
+            string rrr = "1";
+            string inlocuire = rrr.Replace("1", "2");
+            Console.WriteLine(inlocuire);
+
+            //metoda "ToUpper"
+            string lower = "abcd";
+            string up = lower.ToUpper();
+            Console.WriteLine(up);
+
+
+
+
+            //metoda "lastindexof"
+            string last = "aaa bbb ccce";
+            Console.WriteLine(last.LastIndexOf("e"));
+
+            //metoda "startswith"
+            string incepe = "cabcaa";
+            Console.WriteLine(incepe.StartsWith("c"));
+
+            //metoda "substring"
+            string under = "Ihave a string";
+            int val = 4;
+            string Substring = under.Substring(val);
+            Console.WriteLine(val);
+
+        }
+    }
+}
